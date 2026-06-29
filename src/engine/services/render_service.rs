cf898a7cae9db8bcf75f6eb24fa48eed983dac79
Matei Petre <matei139101@gmail.com>
2026-06-29 05:07:00 +0200

    Update: Added the start of the rewrite of Vulkan for the render service.

--- src/engine/services/render_service.rs
+++ src/engine/services/render_service.rs
@@ -1,25 +1,47 @@
 use crate::prelude::*;
 
 /// Handles all rendering related tasks.
+///
+/// This takes care of making the required calls to graphicsAPIs for different rendering processes.
 pub struct RenderService {
-    _graphics_api: Arc<RwLock<GraphicsAPI>>,
+    graphics_api: Arc<RwLock<dyn GraphicsAPI>>,
 }
 
 impl RenderService {
     pub fn new() -> Self {
         Self {
-            _graphics_api: Arc::new(RwLock::new(GraphicsAPI::Vulkan())),
+            graphics_api: Arc::new(RwLock::new(Vulkan::new())),
         }
     }
 }
 
 impl Service for RenderService {
-    fn update(&self) {}
+    fn update(&self) {
+        self.graphics_api.read().unwrap().render_frame();
+    }
 
     fn get_data(&self) {}
 }
 
-enum GraphicsAPI {
-    Vulkan(),
-    _DirectX12(),
+/// A trait a graphicsAPI must implement to be usable by the [`RenderService`]. 
+///
+/// This is to allow for easy addition of new APIs in the future by making graphicsAPI calls more
+/// generic.
+trait GraphicsAPI: Any + Send + Sync {
+    fn render_frame(&self);
+}
+
+/// Defines a struct containing all vulkan functionalities for different processes.
+struct Vulkan {}
+
+impl Vulkan {
+    pub fn new() -> Self {
+        Self {  }
+    }
+}
+
+impl GraphicsAPI for Vulkan {
+    fn render_frame(&self) {
+        todo!();
+    }
 }
