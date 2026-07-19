7ecd561ef8195a1b94e3c6d024ae8e88bd90e133
Matei Petre <matei139101@gmail.com>
2026-07-19 05:38:43 +0200

    Update: Added scheduler thread handling and channel for better threading.

--- src/engine/scheduler.rs
+++ src/engine/scheduler.rs
@@ -1,3 +1,6 @@
+use std::thread::JoinHandle;
+use crossbeam_channel::{Receiver, Sender, unbounded};
+
 use crate::prelude::*;
 
 /// Used for smartly handling threads and asynchronous work between engine services.
@@ -5,25 +8,31 @@ use crate::prelude::*;
 /// Asynchronously handling work can be heavily benificial for engine performance but difficult to
 /// manage. The [`Scheduler`] exists for the sole reason of managing threads and ensuring work gets
 /// distributed between the threads properly.
-pub struct Scheduler {}
+pub struct Scheduler {
+    scheduler_thread: Option<JoinHandle<()>>,
+    scheduler_channel: (Sender<i32>, Receiver<i32>)
+}
 
 impl Scheduler {
     /// Returns a new [`Scheduler`].
-    pub fn new() -> Self {
-        Self {}
+    pub fn new() -> Self {        
+        Self {
+            scheduler_thread: None,
+            scheduler_channel: unbounded()
+        }
     }
 
     /// Sets up required threads for the provided [`ServiceLocator`] and starts them.
     pub fn run(&self, service_locator: &ServiceLocator) {
         log!(Self, Critical, "Starting scheduled threads.");
         for (_service_type, service) in service_locator.iter() {
-            self.start_thread(service.clone());
+            self.start_thread(service.clone(), self.scheduler_channel.1.clone());
         }
         self.start_window_thread();
     }
 
     /// Starts a thread for the provided [`Service`].
-    fn start_thread(&self, service: Arc<dyn Service>) {
+    fn start_thread(&self, service: Arc<dyn Service>, receiver: Receiver<i32>) {
         log!(Self, Critical, "Starting thread.");
         let _handle = thread::spawn(move || loop {
             service.update();
